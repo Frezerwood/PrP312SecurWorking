f7e3bda76528ee977b0045f6be5b707a09b1dee0
Aleksandr Lomakin <samarazoyl@gmail.com>
2023-12-21 15:35:18 +0400

    G&S for U&R

---
+++ src/main/java/ru/kata/spring/boot_security/demo/models/Role.java
@@ -0,0 +1,49 @@
+package ru.kata.spring.boot_security.demo.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "Role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(
+            name = "User_Role",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<User> users;
+
+    public Role() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
