4c7682ba947a74099b8adef23608e17325e7c027
Aleksandr Lomakin <samarazoyl@gmail.com>
2024-01-09 02:52:46 +0400

    something else

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.services.RoleService;
 import ru.kata.spring.boot_security.demo.services.UserService;
 
 import java.security.Principal;
@@ -14,17 +15,19 @@ import java.security.Principal;
 @RequestMapping("/user")
 public class UserController {
 
-
+    private final RoleService roleService;
     private final UserService userService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
         this.userService = userService;
     }
 
     @GetMapping("")
     public String userPage(Principal principal, Model model) {
-        model.addAttribute("userAuth", userService.findByUsername(principal.getName()));
+        model.addAttribute("user", userService.findByUsername(principal.getName()));
+        model.addAttribute("roles", roleService.findAll());
         return "/user";
     }
 }
